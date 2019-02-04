578e336c27f9fe28bbc2949adf7b3479e363eb2d
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-04 23:19:20 +0530

    Add files via upload

---
+++ PrintNtimes.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class PrintNtimes{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+int i=scan.nextInt();
+for(int a=0;a<i;a++){
+System.out.println("Hello");
+}
+}
+}
