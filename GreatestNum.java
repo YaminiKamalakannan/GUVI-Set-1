35df63baf089d7dfaf149540be8307efcab375fc
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-11 20:30:01 +0530

    Update GreatestNum.java

--- GreatestNum.java
+++ GreatestNum.java
@@ -3,19 +3,13 @@ class GreatestNum{
 public static void main(String[] args){
 Scanner x=new Scanner(System.in);
 int a=x.nextInt();
-
 int b=x.nextInt();
-
 int c=x.nextInt();
-
-if(a>b&&a>c){
+if(a>b&&a>c)
 System.out.println(a);
-}
-else if(b>a&&b>c){
+else if(b>a&&b>c)
 System.out.println(b);
-}
-else{
+else
 System.out.println(c);
-}
 }
 }
