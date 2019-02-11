628d9759e1fec92fb95fdaabf2caf1b93391ae9e
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-11 20:28:00 +0530

    Add files via upload

---
+++ GreatestNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class GreatestNum{
+public static void main(String[] args){
+Scanner x=new Scanner(System.in);
+int a=x.nextInt();
+
+int b=x.nextInt();
+
+int c=x.nextInt();
+
+if(a>b&&a>c){
+System.out.println(a);
+}
+else if(b>a&&b>c){
+System.out.println(b);
+}
+else{
+System.out.println(c);
+}
+}
+}
