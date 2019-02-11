f18c372a709a021ffc0afca2f565807d0b2eb39e
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-11 19:45:59 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 class OddEven{
 public static void main(String[] args){
 Scanner abc=new Scanner(System.in);
-System.out.println("Enter a number:");
 int a=abc.nextInt();
 if(a%2==0)
 System.out.println("Even");
