ce33d25e9744f039a7c31aa6692edb1a431e9c25
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-11 19:52:54 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -1,26 +1,11 @@
 import java.util.Scanner;
-
 class Vowel{
-
 public static void main(String[] args){
-
 Scanner abc=new Scanner(System.in);
-
-String c=abc.next();
-
-char input=c.toLowerCase().charAt(0);
-if((input=='a')||(input=='e')||(input=='i')||(input=='o')||(input=='u')){
-
+char input=abc.next().toLowerCase().charAt(0);
+if((input=='a')||(input=='e')||(input=='i')||(input=='o')||(input=='u'))
 System.out.println("Vowel");
-
-}
-
-else{
-
+else
 System.out.println("Consonant");
-
 }
-
-}
-
 }
