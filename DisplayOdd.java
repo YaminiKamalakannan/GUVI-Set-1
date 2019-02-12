d2d6a878ff1dfd46707615e22f54164d7615009e
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-12 21:30:57 +0530

    Add files via upload

---
+++ DisplayOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class DisplayOdd{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int num=0;
+for(int i=a+1;i<b;i++){
+num=i%2;
+if(num==1)
+System.out.println(i);
+}
+}
+}
