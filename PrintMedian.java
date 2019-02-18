08e2dcfd6aa3e855b72351bf7b615c72758b21f0
YaminiKamalakannan <47329619+YaminiKamalakannan@users.noreply.github.com>
2019-02-18 22:36:33 +0530

    Add files via upload

---
+++ PrintMedian.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class PrintMedian{
+
+public static void main(String[] args){
+
+Scanner scan=new Scanner(System.in);
+
+int n=scan.nextInt();
+
+int[] arr=new int[n];
+
+for(int i=0;i<n;i++)
+
+arr[i]=scan.nextInt();
+
+int temp=0,median=0;
+
+
+for(int i=0;i<n;i++){
+
+for(int j=i+1;j<n;j++){
+
+if(arr[i]>arr[j]){
+
+temp=arr[i];
+
+arr[i]=arr[j];
+
+arr[j]=temp;
+
+}
+
+}
+
+}
+
+if(n%2==1)
+median=arr[((n-1)/2)];
+else
+median=arr[n/2];
+
+System.out.print(median);
+
+}
+
+}
